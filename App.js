ab9e7aeb071e46eeda71ef7a44fa3745aa044005
Admin <admin@MacBook-Pro-Admin.local>
2020-01-02 23:51:58 +0200

    flex

--- App.js
+++ App.js
@@ -12,8 +12,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     backgroundColor: '#000',
+
+    // сдвиг право-лево (при flexDirection: column)
+    // сдвиг верх-низ (при flexDirection: row)
     alignItems: 'center',
+
+    // сдвиг верх-низ (при flexDirection: column)
+    // сдвиг право-лево (при flexDirection: row)
     justifyContent: 'center',
   },
   text: {
