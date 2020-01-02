7ad068f284759f152db71d9f3a9399b152aa6b83
Admin <admin@MacBook-Pro-Admin.local>
2020-01-03 00:26:02 +0200

    Создание навбара, его стилизация, передача пропсов в навбар

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Navbar } from './src/Navbar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-
+        <Navbar title='Todo app'/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    
+
   }
 });
---
+++ src/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export const Navbar = props => {
+  return (
+    <View style={styles.navbar}>
+<Text style={styles.text}>{props.title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  navbar: {
+    flexDirection: "column",
+    height: 70,
+    alignItems: "center",
+    justifyContent: "flex-end",
+    backgroundColor: "#3949ab",
+    paddingBottom: 10
+  },
+
+  text: {
+    color: "yellow",
+    fontSize: 15
+  }
+});
