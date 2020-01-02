d99974cb5e2220c7d9c8777871e5a5461e924306
Admin <admin@MacBook-Pro-Admin.local>
2020-01-02 23:54:33 +0200

    begin

--- App.js
+++ App.js
@@ -4,27 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hellodfdfhjh111</Text>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#000',
-
-    // сдвиг право-лево (при flexDirection: column)
-    // сдвиг верх-низ (при flexDirection: row)
-    alignItems: 'center',
-
-    // сдвиг верх-низ (при flexDirection: column)
-    // сдвиг право-лево (при flexDirection: row)
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontSize: 26
+    
   }
 });
