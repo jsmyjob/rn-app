0a507eda029db16c3a7f4c12437b36ed4fd18bc7
Admin <admin@MacBook-Pro-Admin.local>
2020-01-03 03:06:56 +0200

    Проверка работы добавления заданий с заглушкой. Исправлена проблема с одинаковыми ключами - нужно использовать  Date.now(), а не new Date()

--- App.js
+++ App.js
@@ -1,26 +1,39 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Navbar } from "./src/Navbar";
 import { AddToDo } from "./src/AddTodo";
 
 export default function App() {
-
   // todos - стейт, setTodos - функция, меняющая стейт
   const [todos, setTodos] = useState([]); // тип данных "Кортеж"
 
   //*
   //**
   // возврат (раскукоженого старого стейта + новый элемент)
-  const addTodo = (title) => [...todos, {
-     id = new Date().toString(),
-     title: title
-  }]
+
+  const addTodo = (title) => {
+
+    // вызов функции
+    setTodos( (prev) => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title
+      }
+    ])
+  };
+  
 
   return (
-    <View >
+    <View>
       <Navbar title="Todo app" />
       <View style={styles.container}>
-        <AddToDo onSubmit={addTodo}/>
+        <AddToDo onSubmit={addTodo} />
+        <View>
+          {todos.map(todo => {
+            return <Text key={todo.id}>{todo.title}</Text>;
+          })}
+        </View>
       </View>
     </View>
   );
@@ -32,7 +45,6 @@ const styles = StyleSheet.create({
     paddingVertical: 10
   }
 });
-
 
 //* setTodos(todos.concate([newTodo]))
 
