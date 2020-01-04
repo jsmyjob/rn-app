426b38906d3a949f22f2cf5729627a7cc4976876
Admin <admin@MacBook-Pro-Admin.local>
2020-01-04 17:54:58 +0200

    Стилизация кнопок на TodoScreen

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,13 +1,29 @@
 import React from "react";
 import { StyleSheet, View, Text, Button } from "react-native";
+
 export const TodoScreen = ({ goBack, todo }) => {
   return (
     <View style={styles.container}>
       <Text>{todo.title}</Text>
-      <Button title="Назад" onPress={goBack} />
+      <View style={styles.buttonsBlock}>
+        <View style={styles.button} >
+          <Button title="Назад" onPress={goBack} color={'grey'}/>
+        </View>
+        <View style={styles.button}>
+          <Button title="Удалить" onPress={() => console.log("DELETE")} color={'red'}/>
+        </View>
+      </View>
     </View>
   );
 };
+
 const styles = StyleSheet.create({
-  container: {}
+  container: {},
+  buttonsBlock: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  button: {
+    width: "35%"
+  }
 });
