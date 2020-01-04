7c6a3d3f6a614bd31d2bb58c7fa417ef4d33007a
Admin <admin@MacBook-Pro-Admin.local>
2020-01-04 20:16:22 +0200

    Создание ui-компонента карточки задания AppCard

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { StyleSheet, View, Text, Button } from "react-native";
 import { THEME } from "../themes/theme";
+import { AppCard } from "../ui/AppCard";
 
 export const TodoScreen = ({ goBack, todo }) => {
   return (
     <View style={styles.container}>
-      <Text>{todo.title}</Text>
+      <AppCard style={styles.card}>
+        <Text style={styles.title}>{todo.title}</Text>
+        <Button title={'Ред.'}/>
+      </AppCard>
+
       <View style={styles.buttonsBlock}>
         <View style={styles.button}>
           <Button title="Назад" onPress={goBack} color={THEME.GREY_COLOR} />
@@ -28,7 +33,16 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between"
   },
+
+  card: {
+    padding: 15,
+    marginBottom: 20
+  },
+
   button: {
     width: "35%"
-  }
+  },
+  title: {
+    fontSize: 15
+  },
 });
---
+++ src/ui/AppCard.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { THEME } from "../themes/theme";
+
+export const AppCard = props => (
+  <View style={{ ...styles.default, ...props.style }}>
+      {props.children}
+      </View>
+);
+
+const styles = StyleSheet.create({
+  default: {
+    padding: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderRadius: 10,
+
+    // тени
+    shadowColor: "#000",
+    elevation: 8,
+    backgroundColor: "#fff",
+
+    // only IOS shadows
+    shadowRadius: 2,
+    shadowOpacity: 0.3,
+    shadowOffset: { width: 2, height: 2 }
+  }
+});
