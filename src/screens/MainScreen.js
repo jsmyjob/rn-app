f796aee8b433f24dd929cf05042ed57c5362ffe3
Admin <admin@MacBook-Pro-Admin.local>
2020-01-05 06:49:56 +0200

    Добавление картинки, в случает отсутствия заданий

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,21 +1,48 @@
 import React from "react";
-import { StyleSheet, View, FlatList } from "react-native";
+import { StyleSheet, View, FlatList, Image } from "react-native";
 import { AddTodo } from "../components/AddTodo";
 import { Todo } from "../components/Todo";
 export const MainScreen = ({ addTodo, removeTodo, openTodo, todos }) => {
+  let content = (
+    <FlatList
+      keyExtractor={item => item.id}
+      data={todos}
+      renderItem={({ item }) => (
+        <Todo todo={item} onRemove={removeTodo} onOpen={openTodo} />
+      )}
+    />
+  );
+
+  if (todos.length === 0) {
+    content = (
+      <View style={styles.imageWrapper}>
+        <Image
+          style={styles.image}
+          source={require("../../assets/no-items.png")}
+        />
+      </View>
+    );
+  }
   return (
     <View style={styles.container}>
       <AddTodo onSubmit={addTodo} />
-      <FlatList
-        data={todos}
-        renderItem={({ item }) => (
-          <Todo todo={item} onRemove={removeTodo} onOpen={openTodo} />
-        )}
-        keyExtractor={item => item.id}
-      />
+      {content}
     </View>
   );
 };
+
 const styles = StyleSheet.create({
-  container: {}
+  container: {},
+  imageWrapper: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    height: 300
+  },
+  image: {
+    height: '100%',
+    width: '100%',
+    resizeMode: 'contain'
+
+  }
 });
